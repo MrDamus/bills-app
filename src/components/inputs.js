1b2bc54b80d92bf884570ea99f3d9e686013dc7a
Damian <themrdamus@gmail.com>
2018-07-05 20:48:22 +0100

    fixed on change inputs and added list component

--- src/components/inputs.js
+++ src/components/inputs.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { FormGroup, FormControl, ControlLabel, InputGroup, Button } from 'react-bootstrap';
-import { selectValue } from '../redux/actions';
-import { addTopup } from '../redux/actions/addTopup'
+import { FormControl, ControlLabel, InputGroup, Button } from 'react-bootstrap';
+import PickerActions from '../redux/actions';
 
-
-const Inputs = ({ name, selectValue, amount, addTopup }) => (
-  <FormGroup controlId={name} bsSize="large" style={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
-    <ControlLabel style={{textAlign: 'center'}}>{name} </ControlLabel>
+const Inputs = ({ name, selectValue, amount, addTopup, selectType }) => (
+  <form onSubmit={event => event.preventDefault()} style={{display: 'flex', flexDirection: 'column'}}>
+    <ControlLabel style={{textAlign: 'center', fontSize: '30px'}}>{name} </ControlLabel>
     <InputGroup>
       {/* <FormGroup controlId="formControlsSelect"> */}
       <ControlLabel>Select type </ControlLabel>
-      <FormControl componentClass="select" placeholder="select">
+      <FormControl
+        componentClass="select"
+        placeholder="select"
+        onChange={e => selectType(e.target.value)}
+      >
         <option value="Electricity">Electricity</option>
         <option value="Gas">Gas</option>
       </FormControl>
@@ -28,21 +30,23 @@ const Inputs = ({ name, selectValue, amount, addTopup }) => (
         value={amount}
         onChange={e => selectValue(e.target.value)}
       />
+    </InputGroup>
       <Button 
         bsStyle="success"
         block
+        style={{maxWidth: '300px', alignSelf: 'center', marginTop: '10px'}}
         type="submit"
         onClick={addTopup}
         >Confirm
-
       </Button>
-    </InputGroup>
-  </FormGroup>
+  </form>
 );
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    selectValue: (user) => dispatch(selectValue(user)),
+    selectValue: (inputValue) => dispatch(PickerActions.selectValue(inputValue)),
+    selectType: (inputValue) => dispatch(PickerActions.selectType(inputValue)),
+    addTopup: () => dispatch(PickerActions.addTopup()),
   }
 }
 
