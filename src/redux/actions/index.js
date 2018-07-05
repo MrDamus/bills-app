bc71972067e168c7152b9605f71c952f48280e96
Damian <themrdamus@gmail.com>
2018-07-05 20:47:07 +0100

    fixed actions for selecting type and adding

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,3 +2,19 @@ export const selectValue = (payload) => ({
   type: 'SELECT_VALUE',
   payload
 })
+
+export const addTopup = (payload) => ({
+  type: 'ADD_TOPUP',
+  payload
+})
+
+export const selectType = (payload) => ({
+  type: 'SELECT_TYPE',
+  payload
+})
+
+export default {
+  selectValue,
+  addTopup,
+  selectType
+}
