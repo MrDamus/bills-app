796043b3c33f8372ce8f6faacf3cf793ad68d965
Damian <themrdamus@gmail.com>
2018-07-08 11:22:36 +0100

    initialized firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: "AIzaSyCU6KeOyeOs-JtERj6HPo86rLHEulCy-Yg",
+    authDomain: "billssharingapp.firebaseapp.com",
+    databaseURL: "https://billssharingapp.firebaseio.com",
+    projectId: "billssharingapp",
+    storageBucket: "billssharingapp.appspot.com",
+    messagingSenderId: "1054161841961"
+  };
---
+++ src/firebase/firebase.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import { config } from './config'
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+  firebase.auth().signInAnonymously()
+  .then(() => {
+    console.warn('User registered')
+  })
+  .catch(error => {
+    console.warn(error);
+  })
+}
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export {
+  db,
+  auth,
+};
