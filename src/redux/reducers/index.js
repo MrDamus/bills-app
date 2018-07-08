ba7c0e945761a172823aeaa672fe0edf6bb25589
Damian <themrdamus@gmail.com>
2018-07-08 22:51:58 +0100

    added setUser reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import amountPicker from './amountPicker'
 import deal from './deal'
 import group from './group';
+import user from './user';
 
 export default combineReducers({
   amountPicker,
   deal,
-  group
+  group,
+  user
 });
