a528cc7705ac337c855fd6fd143c478523a9f378
Damian <themrdamus@gmail.com>
2018-07-09 18:20:07 +0100

    saving userID in local storage

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,14 +1,13 @@
 import GroupService from "../services/groupService";
 import { db } from '../../firebase';
 
-
 export function setUser(userId) {
   return function(dispatch, getState) {
-    dispatch(() => ({type: 'SET_USER', payload: userId}));
-    localStorage.setItem('userId', JSON.stringify(userId))
+    dispatch(({type: 'SET_USER', payload: userId}));
+    localStorage.clear('userId');
+    localStorage.setItem('userId', userId)
   }
 }
-
 
 export default {
   setUser,
