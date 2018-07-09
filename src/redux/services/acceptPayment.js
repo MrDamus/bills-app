1d8d216a31b4413f44cd0d119ee30b8d7d8e48ea
Damian <themrdamus@gmail.com>
2018-07-09 15:41:59 +0100

    fixed names, colon and propTypes

--- src/redux/services/acceptPayment.js
+++ src/redux/services/acceptPayment.js
@@ -1,6 +1,6 @@
 const SERVER_URL = 'http://localhost:3000/'
 
-function acceptPayment(type. value, name ) {
+function acceptPayment(type, value, name ) {
   return fetch(`${SERVER_URL}`, {
     method: 'POST',
     body: JSON.stringify(payment),
