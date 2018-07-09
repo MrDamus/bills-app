1f818a538f88e23dc05f4a23fbff841e06baa69b
Damian <themrdamus@gmail.com>
2018-07-09 15:39:51 +0100

    reworked setUser action

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,10 +1,14 @@
 import GroupService from "../services/groupService";
 import { db } from '../../firebase';
 
-export const setUser = (payload) => ({
-  type: 'SET_USER',
-  payload
-})
+
+export function setUser(userId) {
+  return function(dispatch, getState) {
+    dispatch(() => ({type: 'SET_USER', payload: userId}));
+    localStorage.setItem('userId', JSON.stringify(userId))
+  }
+}
+
 
 export default {
   setUser,
