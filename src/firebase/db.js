aae9a1621be131d6772afb38d8007af8eec4614b
Damian <themrdamus@gmail.com>
2018-07-10 21:42:16 +0100

    fetching group payments

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -26,10 +26,31 @@ export const onceGetUsers = () =>
   db.collection('users')
     .get();
 
-  export const fetchUserPayments = (userId) =>
+export const fetchUserPayments = (userId) =>
   db.collection('payments')
     .where('requestorId', '==',userId)
     .get();
+
+export const fetchGroupPayments = async (userId) => {
+  const userData = await db.collection('users')
+    .doc(userId)
+    .get();
+
+  const { groupId } = userData.data();
+
+  const groupUsers = await db.collection('users')
+  .where('groupId', '==', groupId)
+  .get();
+  const groupUsersIds = groupUsers.docs.map(g => g.id)
+  const groupPayments = []
+
+  groupUsersIds.forEach(async id => {
+    const payments = await db.collection('payments')
+    .where('requestorId', '==', userId)
+    .get();
+    groupPayments.push([...payments.docs])
+  });
+}
 
 export const addPayment = (amount, type, requestorId) =>
   db.collection(`payments`)
