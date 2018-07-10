2ba111185c47067365e16da71b375d8a27731c3c
Damian <themrdamus@gmail.com>
2018-07-10 23:37:42 +0100

    fixed group usersId in db

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -39,17 +39,21 @@ export const fetchGroupPayments = async (userId) => {
   const { groupId } = userData.data();
 
   const groupUsers = await db.collection('users')
-  .where('groupId', '==', groupId)
-  .get();
+    .where('groupId', '==', groupId)
+    .get();
   const groupUsersIds = groupUsers.docs.map(g => g.id)
   const groupPayments = []
 
-  groupUsersIds.forEach(async id => {
-    const payments = await db.collection('payments')
-    .where('requestorId', '==', userId)
-    .get();
-    groupPayments.push([...payments.docs])
-  });
+  const requests = groupUsersIds.map(id => 
+    db.collection('payments')
+      .where('requestorId', '==', id)
+      .get()
+  );
+  const paymentsDocuments = await Promise.all(requests)
+  const paymentsData = paymentsDocuments.map(a => a.docs)
+  const allRequests = [].concat(...paymentsData)
+  const data = allRequests.map(d => d.data())
+  return data
 }
 
 export const addPayment = (amount, type, requestorId) =>
@@ -59,3 +63,9 @@ export const addPayment = (amount, type, requestorId) =>
     type,
     requestorId,
   });
+
+  // export const approvePayment = (isApproved) =>
+  // db.collection(`payments`)
+  //   .add({
+  //   isApproved,
+  // });
